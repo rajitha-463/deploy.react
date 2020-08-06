35f038d9e0ebde4b82f8d5e8c951ab3e613f01dc
rajitha-463 <gangireddyrajitha68@gmail.com>
2020-08-06 12:36:01 +0530

    Updates

---
+++ precache-manifest.292430706f4d8c42fc4290816211be77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f034a1df3d28816efe56d03f39c9cd",
+    "url": "/deploy.react/index.html"
+  },
+  {
+    "revision": "6010f5f7e25d81d3521f",
+    "url": "/deploy.react/static/css/main.14a7e9ae.chunk.css"
+  },
+  {
+    "revision": "3c69f6531c7f40747f79",
+    "url": "/deploy.react/static/js/2.72cf69d9.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/deploy.react/static/js/2.72cf69d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6010f5f7e25d81d3521f",
+    "url": "/deploy.react/static/js/main.b9bd0730.chunk.js"
+  },
+  {
+    "revision": "138e355dc5bb01b2f37c",
+    "url": "/deploy.react/static/js/runtime-main.b3ce2c6f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/deploy.react/static/media/logo.9f7e506d.svg"
+  }
+]);
